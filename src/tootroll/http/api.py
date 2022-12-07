5cdd0ec4ad787e39d4a3e7a43a1963eb5512442f
anthonyp <anthony.potappel@gmail.com>
2022-12-07 22:32:45 +0100

    wip, improve range support

--- src/tootroll/http/api.py
+++ src/tootroll/http/api.py
@@ -1,6 +1,5 @@
 import os
 import re
-import json
 import logging
 
 import uvicorn
@@ -8,13 +7,12 @@ from fastapi import APIRouter, FastAPI, Request
 from fastapi.responses import Response, HTMLResponse
 from starlette.middleware.cors import CORSMiddleware
 
-from typing import Dict, Tuple, Optional, Any
+from typing import Tuple, Optional
 
+from .responses import json_response, empty_response, static_file_head_response, static_file_get_response
 from ..db.utils import list_by_partition
-
 from ..vars import DATABASE_DIR
 
-HTML_DIR = f"{os.path.dirname(__file__)}/html"
 
 API_PREFIX = "/api/v1"
 ALLOWED_ORIGINS = "*"
@@ -30,87 +28,6 @@ app = FastAPI(
 
 api = APIRouter(prefix=API_PREFIX)
 static = APIRouter(prefix="")
-
-
-def json_response(content: Dict[str, Any], status_code=200) -> Response:
-    return Response(
-        content=json.dumps(content),
-        media_type="application/json",
-        status_code=status_code
-    )
-
-
-def empty_response(status_code: int) -> Response:
-    return Response(
-        content=b"",
-        status_code=status_code
-    )
-
-
-def validate_http_request_range(input_range: str, filesize: int) -> Tuple[int, int]:
-
-    if not re.match("[0-9]{1,}-[0-9]{0,}$|^[0-9]{0,}-[0-9]{1,}$", input_range):
-        raise ValueError(f"invalid range: {input_range}")
-
-    r_start_str, r_end_str = input_range.split("-")
-    if not r_start_str:
-        # e.g. -10, get last 10 bytes)
-        r_start = filesize - int(r_end_str)
-        r_end = filesize - 1
-    elif not r_end_str:
-        # e.g. 10-, start at byte #10
-        r_start = int(r_start_str)
-        r_end = filesize - 1
-    else:
-        r_start = int(r_start_str)
-        r_end = int(r_end_str)
-
-    if r_start < 0 or r_start >= filesize:
-        raise ValueError(f"invalid range, {input_range} (filesize={filesize}")
-
-
-    return tuple([int(r_start), int(r_end)])
-
-
-def static_file_response(file_path: str, range_request: Optional[str] = None) -> HTMLResponse:
-    if not os.path.exists(file_path):
-        return empty_response(404)
-
-    if not range_request:
-        with open(file_path, "rb") as ifile:
-            content = ifile.read()
-        return HTMLResponse(content=content, status_code=200)
-    else:
-        file_info = os.stat(file_path)
-        range_type, ranges_str = range_request.strip(" ").split("=")
-        # TODOs: 
-        # give a proper error if range_type != bytes
-
-        assert range_type == "bytes"
-        try:
-            ranges = [
-                validate_http_request_range(input_range, file_info.st_size)
-                for input_range in list(
-                    [r.strip(" ") for r in ranges_str.split(",")
-                ])
-            ]
-        except ValueError as error:
-            return HTMLResponse(content=str(error), status_code=400)
-
-        for r_start, r_end in ranges:
-            fo = open(file_path, "rb")
-            fo.seek(int(r_start))
-            content = fo.read(int(r_end) - int(r_start) + 1)
-            fo.close()
-
-            headers = {
-                "Content-Range": f"bytes {range_request}/{file_info.st_size}",
-                "Content-Type": "application/octet-stream",
-                "Content-Length": str(len(content)),
-            }
-            # TODO: # parse multiple ranges (Content-Type: multipart/byteranges; boundary=String_separator)
-            break
-        return Response(content=content, headers=headers, status_code=206)
 
 
 @api.get("/alive", status_code=200)
@@ -213,22 +130,23 @@ async def list_partition_files(
 async def timelines_home_options(request: Request, rest_of_path: str) -> Response:
     response = Response()
     response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS, HEAD'
     response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
     return response
 
 
+import json
 # catch any other file in non-API_PREFIX path
-@static.get("/{url_path:path}", response_class=HTMLResponse)
-async def static_file(url_path: str, request: Request) -> HTMLResponse:
+@static.head("/{url_path:path}", response_class=HTMLResponse)
+async def static_file_head(url_path: str, request: Request) -> HTMLResponse:
+    print( json.dumps(request.headers, indent=4, default=str))
+    return static_file_head_response(url_path, range_request=request.headers.get("Range", None))
 
-    if re.match("^db/.*/.*\.parquet/.*/.*\.parquet", url_path):
-        file_path = f'{DATABASE_DIR}/{url_path.split("db/", 1)[-1]}'
-    else:
-        file_path = f"{HTML_DIR}/{url_path}"
-        if file_path[-1] == "/":
-            file_path += "index.html"
-    return static_file_response(file_path, range_request=request.headers.get("Range", None))
+
+@static.get("/{url_path:path}", response_class=HTMLResponse)
+async def static_file_get(url_path: str, request: Request) -> HTMLResponse:
+    print( json.dumps(request.headers, indent=4, default=str))
+    return static_file_get_response(url_path, range_request=request.headers.get("Range", None))
 
 
 def app_main():
---
+++ src/tootroll/http/responses.py
@@ -0,0 +1,146 @@
+import os
+import re
+import json
+import mmap
+
+from fastapi.responses import Response, HTMLResponse
+
+from typing import Dict, List, Tuple, Optional, Any
+
+from ..vars import DATABASE_DIR
+
+HTML_DIR = f"{os.path.dirname(__file__)}/html"
+
+
+def json_response(content: Dict[str, Any], status_code=200) -> Response:
+    return Response(
+        content=json.dumps(content),
+        media_type="application/json",
+        status_code=status_code
+    )
+
+
+def empty_response(status_code: int) -> Response:
+    return Response(
+        content=b"",
+        status_code=status_code
+    )
+
+
+def validate_http_request_range(input_range: str, filesize: int) -> Tuple[int, int]:
+
+    if not re.match("[0-9]{1,}-[0-9]{0,}$|^[0-9]{0,}-[0-9]{1,}$", input_range):
+        raise ValueError(f"invalid range: {input_range}")
+
+    r_start_str, r_end_str = input_range.split("-")
+    if not r_start_str:
+        # e.g. -10, get last 10 bytes)
+        r_start = filesize - int(r_end_str)
+        r_end = filesize - 1
+    elif not r_end_str:
+        # e.g. 10-, start at byte #10
+        r_start = int(r_start_str)
+        r_end = filesize - 1
+    else:
+        r_start = int(r_start_str)
+        r_end = int(r_end_str)
+
+    if r_start < 0 or r_start >= filesize:
+        raise ValueError(f"invalid range, {input_range} (filesize={filesize}")
+
+
+    return tuple([int(r_start), int(r_end)])
+
+
+def path_url_to_file(url_path: str) -> Tuple[str, str]:
+    if re.match("^db/.*/.*\.parquet/.*/.*\.parquet", url_path):
+        file_path = f'{DATABASE_DIR}/{url_path.split("db/", 1)[-1]}'
+        content_type = "application/octet-stream"
+    else:
+        file_path = f"{HTML_DIR}/{url_path}"
+        if file_path[-1] == "/":
+            file_path += "index.html"
+        content_type = "text/html"
+    return file_path, content_type
+
+
+def static_file_head_response(url_path: str, range_request: Optional[str] = None) -> Response:
+    file_path, content_type = path_url_to_file(url_path)
+
+    if not os.path.exists(file_path):
+        return empty_response(404)
+
+    file_info = os.stat(file_path)
+    if not range_request:
+        headers = {
+            "Content-Type": content_type,
+            "Content-Length": str(file_info.st_size),
+            "Accept-Ranges": "bytes",
+        }
+        return Response(content=b"", headers=headers, status_code=200)
+    # else:
+    try:
+        ranges = parse_range_request(range_request, file_info.st_size)
+    except ValueError as error:
+        return Response(content=str(error), status_code=400)
+
+    headers = {
+        "Content-Type": content_type,
+        "Content-Length": str(file_info.st_size),
+        "Accept-Ranges": "bytes",
+        "Content-Range": f"{range_request}/{file_info.st_size}",
+    }
+    return Response(content=b"", headers=headers, status_code=206)
+
+
+def parse_range_request(range_request: str, file_size: int) -> List[Tuple[int, int]]:
+    range_type, ranges_str = range_request.strip(" ").split("=")
+    if range_type.lower() != "bytes":
+        raise ValueError("Only support ranges of bytes")
+
+    return [
+        validate_http_request_range(input_range, file_size)
+        for input_range in list(
+            [r.strip(" ") for r in ranges_str.split(",")
+        ])
+    ]
+
+
+def static_file_get_response(url_path: str, range_request: Optional[str] = None) -> HTMLResponse:
+
+    file_path, content_type = path_url_to_file(url_path)
+    if not os.path.exists(file_path):
+        return empty_response(404)
+
+    if not range_request:
+        with open(file_path, "rb") as ifile:
+            content = ifile.read()
+        return HTMLResponse(content=content, status_code=200)
+    # else:
+    file_info = os.stat(file_path)
+
+    try:
+        ranges = parse_range_request(range_request, file_info.st_size)
+    except ValueError as error:
+        return Response(content=str(error), status_code=400)
+
+    content = b""
+    with open(file_path, "r+b") as f:
+        # map whole file
+        mm = mmap.mmap(f.fileno(), 0)
+
+        for r_start, r_end in ranges:
+            mm.seek(int(r_start))              
+            content = mm.read(int(r_end) - int(r_start) + 1)
+
+            # TODO: # parse multiple ranges (Content-Type: multipart/byteranges; boundary=String_separator)
+            break
+
+        mm.close()
+
+    headers = {
+        "Content-Range": f"{range_request}/{file_info.st_size}",
+        "Content-Type": content_type,
+        "Content-Length": str(len(content)),
+    }
+    return Response(content=content, headers=headers, status_code=206)
